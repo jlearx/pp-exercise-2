0982da7dbb1c0a24c06c651769f2357cd663b414
U-AMR\jlearx <jlearx@jlearx-MOBL1.amr.corp.intel.com>
2017-08-28 16:49:17 -0700

    Final commit.

--- src/main.py
+++ src/main.py
@@ -7,4 +7,22 @@ Created on Aug 28, 2017
 '''
 
 if __name__ == '__main__':
-    print("Hello World!")
+    number = int(input("Please enter a number: "))
+    
+    if (number % 4 == 0):
+        print("The number is divisible by four.")
+    elif(number % 2 == 0):
+        print("The number is even.")
+    else:
+        print("The number is odd.")
+        
+    numStr = input("Please enter a number to check: ")
+    num = int(numStr)
+    checkStr = input("Please enter the number to divide by: ")
+    check = int(checkStr)
+    
+    if (num % check == 0):
+        print(checkStr + " evenly goes into " + numStr + ".")
+    else:
+        print(checkStr + " does not evenly go into " + numStr + ".")
+        
